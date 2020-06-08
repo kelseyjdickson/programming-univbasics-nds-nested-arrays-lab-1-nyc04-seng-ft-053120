0e61ef7f1104b931c2c85152c27b8f2cd357d218
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-08 13:38:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,8 +28,7 @@ def assembled_matrix
       "Oranges",
       "Watermelon",
       "Eggplant"
-    ],
-    ORGANIC_PRODUCE = [
+    ],ORGANIC_PRODUCE = [
       "Strawberries",
       "Potatoes",
       "Grapes",
