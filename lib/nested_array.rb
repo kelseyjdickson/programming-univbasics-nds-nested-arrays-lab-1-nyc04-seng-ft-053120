30b480c4fe2d2107911f4b311b893648c96b55d7
Kelsey Dickson <kelseymedved@gmail.com>
2020-06-08 14:13:58 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -52,7 +52,7 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-  matrix[row]= new_value
+  matrix[column][row]= new_value
   # binding.pry
-  new_value
+
 end
